613416283596b7fa132a0d1ecac0b015fd04feb9
joelnitta <joelnitta@gmail.com>
2020-10-27 16:17:28 +0900

    Use xargs for parallel running align_hybpip_bbmap in parallel

--- R/run_align_hybpip_bbmap.sh
+++ R/run_align_hybpip_bbmap.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-N=10
-(
-while read args; do
-  ((i=i%N)); ((i++==0)) && wait
-  R/align_hybpip_bbmap.sh "$args" &
-done < working/align_hybpiper_args.txt
-)
+cat working/align_hybpiper_args.txt | xargs -P10 -n1 bash -c 'R/align_hybpip_bbmap.sh "$1"' {}
