c261408c732eb3789a66c7f6f9181f1bd909abdd
joelnitta <joelnitta@gmail.com>
2020-10-27 12:11:51 +0900

    Add script to align hybpiper reads with bbmap

---
+++ R/align_hybpip_bbmap.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Example input argument
+# arg="UFG_393201_P03_WA05:atpF"
+
+# Split input into sample and gene, separated by colon
+SAMPLE=$(echo $1 | cut -d ":" -f 1)
+GENE=$(echo $1 | cut -d ":" -f 2)
+
+# Extract name of reference bait matched by BLASTX
+# (e.g., MH173078-psaA)
+REF=`cat ~/ftol/intermediates/hybpiper/$SAMPLE/$GENE/${GENE}_baits.fasta | head -n 1 | grep -o '[^>]*'`
+
+# Extract path to interleaved fasta file
+# (path formatted for running inside docker container)
+FASTA=/data/intermediates/hybpiper/$SAMPLE/$GENE/${GENE}_interleaved.fasta
+
+# Format name of SAM output
+SAM=${SAMPLE}-${GENE}.sam
+
+# Format name of consenus FASTA output
+CON=${SAMPLE}-${GENE}-con.fasta
+
+# Run BBMAP
+docker run --rm -w /data/intermediates/bbmap --user root \
+  -v /home/joelnitta/ftol/:/data \
+  quay.io/biocontainers/bbmap:38.87--h1296035_0 bbmap.sh \
+  in=$FASTA \
+  out=$SAM \
+  ref=/data/intermediates/ref_dna/$REF.fasta \
+  \nodisk
+
+# activate kindel
+source ~/miniconda2/etc/profile.d/conda.sh
+conda activate kindel
+
+# Extract consensus from SAM with min-depth of 1
+kindel consensus ~/ftol/intermediates/bbmap/$SAM --min-depth 1 >  ~/ftol/intermediates/bbmap/$CON
+
+# Remove temporary files
+rm ~/ftol/intermediates/bbmap/$SAM
--- R/run_align_hybpip_bbmap.sh
+++ R/run_align_hybpip_bbmap.sh
@@ -4,6 +4,6 @@ N=4
 (
 while read args; do
   ((i=i%N)); ((i++==0)) && wait
-  R/align_hybpip.sh "$args" &
+  R/align_hybpip_bbmap.sh "$args" &
 done < working/align_hybpiper_args_short.txt
 )
