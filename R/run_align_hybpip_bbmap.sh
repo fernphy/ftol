d916d74250e85671badb3b23e8b24cf7c5e8c761
Joel Nitta <joelnitta@gmail.com>
2020-10-27 17:20:57 +0900

    Increase hybpip_bbmap threads to 20

--- R/run_align_hybpip_bbmap.sh
+++ R/run_align_hybpip_bbmap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat working/align_hybpiper_args.txt | xargs -P10 -n1 bash -c 'R/align_hybpip_bbmap.sh "$1"' {}
+cat working/align_hybpiper_args.txt | xargs -P20 -n1 bash -c 'R/align_hybpip_bbmap.sh "$1"' {}
