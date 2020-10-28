dd75b137512beb2cf3a0ed6d714920e742e1397a
joelnitta <joelnitta@gmail.com>
2020-10-28 12:23:07 +0900

    Refactor align_hybpip_bbmap.sh to output (and mount) subfolders

--- R/align_hybpip_bbmap.sh
+++ R/align_hybpip_bbmap.sh
@@ -2,13 +2,16 @@
 
 # This aligns short reads that were sorted by HybPiper with BLASTX against
 # reference "target" gene sequences. Assumes hybpiper has been run with output
-# to ./intermediates/hybpiper, and all reference sequences available
-# (one gene per fasta file) in ./intermediates/ref_dna.
+# to $WDIR/intermediates/hybpiper, and all reference sequences available
+# (one gene per fasta file) in $WDIR/intermediates/ref_dna.
 
-# Example input argument (sample name, gene name, separated by colon)
-# arg="UFG_393201_P03_WA05:atpF"
+# Will output results to $WDIR/intermediates/bbmap
 
-# Working directory is /home/joelnitta/ftol
+# Takes a single input argument.
+# Example: (sample name, gene name, separated by colon)
+# UFG_393201_P03_WA05:atpF
+
+# Set and enter working directory
 WDIR=/home/joelnitta/ftol
 cd $WDIR
 
@@ -22,14 +25,18 @@ REF=`cat $WDIR/intermediates/hybpiper/$SAMPLE/$GENE/${GENE}_baits.fasta | head -
 
 # Extract path to interleaved fasta file
 # (path formatted for running inside docker container)
-# FASTA=/data/intermediates/hybpiper/$SAMPLE/$GENE/${GENE}_interleaved.fasta
 FASTA=/data/hybpiper_sample/$GENE/${GENE}_interleaved.fasta
 
 # Format name of SAM output
 SAM=${SAMPLE}-${GENE}.sam
 
 # Format name of consenus FASTA output
-CON=${SAMPLE}-${GENE}-con.fasta
+CON=${SAMPLE}-${GENE}.fasta
+
+# This will create a lot of files (two per sample/gene combination) when we loop.
+# To avoid hitting docker's file number limit, only mount subfolders.
+# (don't issue an error, and don't create a new folder, if one already exists)
+mkdir -p $WDIR/intermediates/bbmap/${SAMPLE}-${GENE}
 
 # Map reads to reference with BBMAP, direct output to log named by sample + gene.
 # Outputs in SAM format.
@@ -37,18 +44,17 @@ CON=${SAMPLE}-${GENE}-con.fasta
 # -Xmx5g: use 1g memory per job (java argument)
 # \nodisk: don't write the index files to disk
 # bbmap prints to stderr, so to log output use `2>`
-docker run --rm -w /data/bbmap --user root \
+docker run --rm -w /data/bbmap/${SAMPLE}-${GENE} --user root \
   -v $WDIR/intermediates/hybpiper/$SAMPLE:/data/hybpiper_sample \
   -v $WDIR/intermediates/ref_dna:/data/ref_dna \
-  -v $WDIR/intermediates/bbmap:/data/bbmap \
-  --ulimit nofile=90000:90000 \
+  -v $WDIR/intermediates/bbmap/${SAMPLE}-${GENE}:/data/bbmap/${SAMPLE}-${GENE} \
   quay.io/biocontainers/bbmap:38.87--h1296035_0 bbmap.sh \
   in=$FASTA \
   out=$SAM \
   t=1 \
   -Xmx1g \
   ref=/data/ref_dna/$REF.fasta \
-  \nodisk 2> ./intermediates/bbmap/${SAMPLE}-${GENE}-log.txt
+  \nodisk 2> $WDIR/intermediates/bbmap/${SAMPLE}-${GENE}/bbmap.log
 
 # Activate kindel
 # assumes miniconda2 has been installed, and a conda environment
@@ -57,7 +63,6 @@ source ~/miniconda2/etc/profile.d/conda.sh
 conda activate kindel
 
 # Extract consensus from SAM with min-depth of 1, trim Ns from alignment ends
-kindel consensus $WDIR/intermediates/bbmap/$SAM --min-depth 1 -t >  $WDIR/intermediates/bbmap/$CON
-
-# Remove SAM file, since we only want the consensus FASTA
-rm -f $WDIR/intermediates/bbmap/$SAM
+kindel consensus $WDIR/intermediates/bbmap/${SAMPLE}-${GENE}/$SAM --min-depth 1 -t \
+  > $WDIR/intermediates/bbmap/${SAMPLE}-${GENE}/$CON \
+  2> $WDIR/intermediates/bbmap/${SAMPLE}-${GENE}/kindel.log
