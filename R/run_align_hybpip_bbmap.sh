43322e2ce25551fc35bda94554a1a3c1877c3eb6
joelnitta <joelnitta@gmail.com>
2020-10-28 15:06:09 +0900

    Use gnu parallel to run mapping in parallel

--- R/run_align_hybpip_bbmap.sh
+++ R/run_align_hybpip_bbmap.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-cat working/align_hybpiper_args.txt | xargs -P20 -n1 bash -c 'R/align_hybpip_bbmap.sh "$1"' {}
+# Run align_hybpip_bbmap.sh in parallel over 20 cores
+# requires gnu parallel to be installed.
+parallel -a working/align_hybpiper_args.txt \
+  --eta \
+  -j 20 \
+  --max-args 1 \
+  --load 80% \
+  --noswap \
+  R/align_hybpip_bbmap.sh
