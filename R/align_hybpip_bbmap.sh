9cbf0475b082e090180d68ecca15f5ad465af0f7
Joel Nitta <joelnitta@gmail.com>
2020-10-28 09:01:20 +0900

    Only mount sample folder within hybpiper instead of full hybpiper output, to avoid maxing out number of files when mounting volume

--- R/align_hybpip_bbmap.sh
+++ R/align_hybpip_bbmap.sh
@@ -8,7 +8,9 @@
 # Example input argument (sample name, gene name, separated by colon)
 # arg="UFG_393201_P03_WA05:atpF"
 
-# Working directory is /home/joelnitta/ftol/
+# Working directory is /home/joelnitta/ftol
+WDIR=/home/joelnitta/ftol
+cd $WDIR
 
 # Split input into sample and gene, separated by colon
 SAMPLE=$(echo $1 | cut -d ":" -f 1)
@@ -16,11 +18,12 @@ GENE=$(echo $1 | cut -d ":" -f 2)
 
 # Extract name of reference bait matched by BLASTX in hybpiper
 # (e.g., MH173078-psaA)
-REF=`cat ~/ftol/intermediates/hybpiper/$SAMPLE/$GENE/${GENE}_baits.fasta | head -n 1 | grep -o '[^>]*'`
+REF=`cat $WDIR/intermediates/hybpiper/$SAMPLE/$GENE/${GENE}_baits.fasta | head -n 1 | grep -o '[^>]*'`
 
 # Extract path to interleaved fasta file
 # (path formatted for running inside docker container)
-FASTA=/data/intermediates/hybpiper/$SAMPLE/$GENE/${GENE}_interleaved.fasta
+# FASTA=/data/intermediates/hybpiper/$SAMPLE/$GENE/${GENE}_interleaved.fasta
+FASTA=/data/hybpiper_sample/$GENE/${GENE}_interleaved.fasta
 
 # Format name of SAM output
 SAM=${SAMPLE}-${GENE}.sam
@@ -31,25 +34,30 @@ CON=${SAMPLE}-${GENE}-con.fasta
 # Map reads to reference with BBMAP, direct output to log named by sample + gene.
 # Outputs in SAM format.
 # -t: number of threads
-# -Xmx5g: use 5g memory per job (java argument)
+# -Xmx5g: use 1g memory per job (java argument)
 # \nodisk: don't write the index files to disk
 # bbmap prints to stderr, so to log output use `2>`
-docker run --rm -w /data/intermediates/bbmap --user root \
-  -v /home/joelnitta/ftol/:/data \
+docker run --rm -w /data/bbmap --user root \
+  -v $WDIR/intermediates/hybpiper/$SAMPLE:/data/hybpiper_sample \
+  -v $WDIR/intermediates/ref_dna:/data/ref_dna \
+  -v $WDIR/intermediates/bbmap:/data/bbmap \
+  --ulimit nofile=90000:90000 \
   quay.io/biocontainers/bbmap:38.87--h1296035_0 bbmap.sh \
   in=$FASTA \
   out=$SAM \
   t=1 \
-  -Xmx5g \
-  ref=/data/intermediates/ref_dna/$REF.fasta \
+  -Xmx1g \
+  ref=/data/ref_dna/$REF.fasta \
   \nodisk 2> ./intermediates/bbmap/${SAMPLE}-${GENE}-log.txt
 
 # Activate kindel
+# assumes miniconda2 has been installed, and a conda environment
+# called 'kindel' already created
 source ~/miniconda2/etc/profile.d/conda.sh
 conda activate kindel
 
 # Extract consensus from SAM with min-depth of 1, trim Ns from alignment ends
-kindel consensus ~/ftol/intermediates/bbmap/$SAM --min-depth 1 -t >  ~/ftol/intermediates/bbmap/$CON
+kindel consensus $WDIR/intermediates/bbmap/$SAM --min-depth 1 -t >  $WDIR/intermediates/bbmap/$CON
 
 # Remove SAM file, since we only want the consensus FASTA
-rm -f ~/ftol/intermediates/bbmap/$SAM
+rm -f $WDIR/intermediates/bbmap/$SAM
