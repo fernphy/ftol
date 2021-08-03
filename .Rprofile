4eabc7c659afdef6580e602ad685178ace869131
Joel Nitta <joelnitta@gmail.com>
2021-08-03 17:20:06 +0900

    Resolve conflicts in Rproflie
    load fs in packages

--- .Rprofile
+++ .Rprofile
@@ -1,1 +1,7 @@
 source("renv/activate.R")
+
+# Resolve conflicts
+conflicted::conflict_prefer("filter", "dplyr")
+conflicted::conflict_prefer("select", "dplyr")
+conflicted::conflict_prefer("gather", "tidyr")
+conflicted::conflict_prefer("map", "purrr")
--- R/packages.R
+++ R/packages.R
@@ -3,10 +3,5 @@ library(drake)
 library(assertr)
 library(glue)
 library(ape)
+library(fs)
 library(tidyverse)
-
-# Resolve conflicts
-conflict_prefer("filter", "dplyr")
-conflict_prefer("select", "dplyr")
-conflict_prefer("gather", "tidyr")
-conflict_prefer("map", "purrr")
