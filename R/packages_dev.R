c000e62b67390d086f0709c7313ec13833998aee
Joel Nitta <joelnitta@gmail.com>
2022-01-14 21:53:50 +0000

    Update dev packages, take renv snapshot

--- R/packages_dev.R
+++ R/packages_dev.R
@@ -1,7 +1,9 @@
 # Packages not loaded when running code,
 # but listed here so renv knows to track them
-library(clustermq)
-library(visNetwork)
+library(clustermq) # for running targets in parallel
+library(visNetwork) # for visualizing targets plan
 library(languageserver) # for R in VS-Code
 library(vscDebugger) # for R in VS-Code
 library(docthis) # for documenting functions
+library(gittargets) # for tracking targes cache with git
+library(BiocManager) # for installing Bioconductor packages
