a70c752aef4f9299a0cef32632f1e2972652e5b5
Joel Nitta <joelnitta@gmail.com>
2022-06-23 08:21:18 +0000

    Update registered files

--- R/register.R
+++ R/register.R
@@ -16,10 +16,14 @@ register("https://github.com/wtesto/4000fernPhylogeny/raw/main/supplementaryTabl
 register("https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump_archive/taxdmp_2022-05-01.zip") # nolint
 # "hash://sha256/883f9d06034178602ada5cff7790903495e9d8e89860aedef7749f931f9c5a23" # nolint
 
-# - fern fossil data: fern_fossils.csv v1.0.0
-register("https://raw.githubusercontent.com/fernphy/ferncal/6beb6b59c007d94d58f35658732ff561a9d6a537/fern_fossils.csv") # nolint
-# "hash://sha256/55fd2f21d8e26e4604d9128871f9435ede08f75efc8ae64ce56c671f8d605a1e" # nolint
+# - fern fossil data: fern_fossils.csv v1.0.1
+register("https://raw.githubusercontent.com/fernphy/ferncal/34a16347db3cf068bbe46d3ecfad78ac98b0676e/fern_fossils.csv") # nolint
+# "hash://sha256/153139fbb560442ad46770a04be370f5884cd6396e0eaf05d6875513f80d072c" # nolint
 
 # - reference alignments on FigShare: ref_aln.tar.gz
-register("https://figshare.com/ndownloader/files/34604057")
-# "hash://sha256/3c37fb9478a8d6d1d5cf12652f01e04c3187db64923be824ca689d924facde18" #nolint
+register("https://figshare.com/ndownloader/files/36005603")
+# "hash://sha256/388b53201a8626d4b41851e716505e7904d24ee3730de25310cb82cd3a1e6e71" #nolint
+
+# - restez db on FigShare: restez_sql_db.tar.gz
+register("https://figshare.com/ndownloader/files/36005606")
+#  "hash://sha256/8059a845c6570eeffb6fe08c29e178a9dc223ab6f929a1b6c6b374e160f21410" #nolint
