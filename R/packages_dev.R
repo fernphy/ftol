960d0134f296fa860b363a040f980a1dce8de688
Joel Nitta <joelnitta@gmail.com>
2023-07-28 15:55:32 +0900

    Update renv and all R packages

--- R/packages_dev.R
+++ R/packages_dev.R
@@ -1,5 +1,6 @@
 # Packages not loaded when running code,
 # but listed here so renv knows to track them
+library(conflicted)
 library(clustermq) # for running targets in parallel
 library(visNetwork) # for visualizing targets plan
 library(languageserver) # for R in VS-Code
@@ -10,7 +11,6 @@ library(gittargets) # for tracking targes cache with git
 library(BiocManager) # for installing Bioconductor packages
 library(here) # used in reports
 library(devtools) # used by restrez
-library(MonetDBLite)  # used by restrez
 library(mirai) # parallelization
 library(nanonext) # parallelization
 library(crew) # parallelization
