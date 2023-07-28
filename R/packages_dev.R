3ddb98dacda63a5c92b5c0c1f15fa55e2cd9c886
Joel Nitta <joelnitta@gmail.com>
2023-07-28 10:41:21 +0900

    Update renv, restez

--- R/packages_dev.R
+++ R/packages_dev.R
@@ -4,6 +4,7 @@ library(clustermq) # for running targets in parallel
 library(visNetwork) # for visualizing targets plan
 library(languageserver) # for R in VS-Code
 library(vscDebugger) # for R in VS-Code
+library(jsonlite) # for R in VS-Code
 library(docthis) # for documenting functions
 library(gittargets) # for tracking targes cache with git
 library(BiocManager) # for installing Bioconductor packages
