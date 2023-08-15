c0677953d1409817f79951611981aa73ad9e66cb
Joel Nitta <joelnitta@gmail.com>
2023-08-16 08:49:40 +0900

    Bump docker version, run as root

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Run workflow and pass in current docker image tag
-IMAGE_TAG=joelnitta/ftol:1.2.0
-docker run --rm -v ${PWD}:/wd -w /wd --env IMAGE_TAG=$IMAGE_TAG $IMAGE_TAG Rscript -e "targets::tar_make()"
+IMAGE_TAG=joelnitta/ftol:1.5.0
+docker run --user root --rm -v ${PWD}:/wd -w /wd --env IMAGE_TAG=$IMAGE_TAG $IMAGE_TAG Rscript -e "targets::tar_make()"
