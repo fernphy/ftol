dfdd849e1890df60d7e97ca907a6490393f53d8c
Joel Nitta <joelnitta@gmail.com>
2023-12-08 17:02:43 +0900

    Update docker tag

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Run workflow and pass in current docker image tag
-IMAGE_TAG=joelnitta/ftol:1.5.0
-docker run --user root --rm -v ${PWD}:/wd -w /wd --env IMAGE_TAG=$IMAGE_TAG $IMAGE_TAG Rscript -e "targets::tar_make()"
+IMAGE_TAG=joelnitta/ftol:1.5.1
+docker run -dt --user root --rm -v ${PWD}:/wd -w /wd --env IMAGE_TAG=$IMAGE_TAG $IMAGE_TAG Rscript -e "targets::tar_make()"
