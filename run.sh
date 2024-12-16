03a3741b9e0d73eafb9cd081a829becd965fbf32
joelnitta <joelnitta@gmail.com>
2024-12-16 11:15:44 +0900

    Bump docker image ver

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Run workflow and pass in current docker image tag
-IMAGE_TAG=joelnitta/ftol:1.7.0
+IMAGE_TAG=joelnitta/ftol:1.8.0
 docker run -dt --user root --rm -v ${PWD}:/wd -w /wd --env IMAGE_TAG=$IMAGE_TAG $IMAGE_TAG Rscript -e "targets::tar_make()"
